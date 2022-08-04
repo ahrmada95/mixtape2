584e27e04df2268fe2a528c5854cbedb4caeebf6
Ahnaf <ahrmada95@gmail.com>
2022-08-04 19:01:19 -0400

    finished css for login screen 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import Login from "./components/LoginComponents/Login";
 
+//GLOBAL VARIABLES FOR USER CACHING
+//login status 
+if(localStorage.getItem('LOGIN_STATUS') === 0) {
+  localStorage.setItem('LOGIN_STATUS', JSON.stringify(true)); //if true, the user has been logged in, false is logged out
+}
+
+//setting user id for caching
+if(localStorage.getItem('USER_ID') === 0){
+  localStorage.setItem('USER_ID', JSON.stringify(0)); //set to default value for server
+}
+
 function App() {
   
   return (
